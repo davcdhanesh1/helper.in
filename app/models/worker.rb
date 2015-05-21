70a2a4b02b902b1d39ace3c02ddb14a77c38acf0
davcdhanesh1 <davcdhanesh1@gmail.com>
2015-05-21 14:31:35 +0530

    Validation for worker's expertise

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,24 +1,12 @@
 class Worker < ActiveRecord::Base
-  validates :firstname, presence: true
-  validates :sirname, presence: true
-  validates :age, numericality: { greater_than_or_equal_to: 18, less_than_or_equal_to: 80 }, presence: true
-  validates :educational_background, presence: true
-  validates :work_background, presence: true
-  validates :native_city, presence: true
-  validates :expertise, presence: true
-
-  has_one :contact_information
-  accepts_nested_attributes_for :contact_information
-
-  has_one :verification_document
-  accepts_nested_attributes_for :verification_document
-
-  has_attached_file :profile_picture, :styles => {:medium => "400x500", :thumb => "100x100"}
-  validates_attachment_content_type :profile_picture, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_presence :profile_picture
+  extend ApplicationHelper
 
   class << self
-    def all_expertise
+    def accepted_expertise 
+      get_values_from_options_for_selection(expertise_options)
+    end
+
+    def expertise_options
       [['Cook', 'cook'],
        ['House maid', 'house-maid'],
        ['Plumber', 'plumber'],
@@ -42,4 +30,23 @@ class Worker < ActiveRecord::Base
       ]
     end
   end
+
+  validates :firstname, presence: true
+  validates :sirname, presence: true
+  validates :age, numericality: { greater_than_or_equal_to: 18, less_than_or_equal_to: 80 }, presence: true
+  validates :educational_background, presence: true
+  validates :work_background, presence: true
+  validates :native_city, presence: true
+  validates :expertise, presence: true, inclusion: {in: accepted_expertise }
+
+  has_one :contact_information
+  accepts_nested_attributes_for :contact_information
+
+  has_one :verification_document
+  accepts_nested_attributes_for :verification_document
+
+  has_attached_file :profile_picture, :styles => {:medium => "400x500", :thumb => "100x100"}
+  validates_attachment_content_type :profile_picture, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :profile_picture
+
 end
